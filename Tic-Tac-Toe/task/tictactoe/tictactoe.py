253c0e6ad2df12cab7576924e4f943f0ad4c18dd
drtierney <d.r.tierney@hotmail.co.uk>
2021-04-24 18:34:05 +0100

    Stage #2 completed
    Stage 2/5: The user is the gamemaster

--- Tic-Tac-Toe/task/tictactoe/tictactoe.py
+++ Tic-Tac-Toe/task/tictactoe/tictactoe.py
@@ -1,3 +1,7 @@
-print("X O X")
-print("O X O")
-print("X X O")
+game_state = input("Enter cells: ")
+
+print("---------")
+print("| " + " ".join(game_state[0:3]) + " |")
+print("| " + " ".join(game_state[3:6]) + " |")
+print("| " + " ".join(game_state[6:9]) + " |")
+print("---------")
